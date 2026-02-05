d3d8094ab4def0b3677efd8caead7a6d38548051
Stubberson <tuukka.j.korpela@gmail.com>
2026-02-05 16:45:09 +0100

    Add grid definition for user

--- script.js
+++ script.js
@@ -6,13 +6,36 @@ function hoverHighlight(square) {
     })
 }
 
-// Create a 16x16 grid
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div')
-    container.appendChild(row)
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement('div')
-        row.appendChild(square)
-        hoverHighlight(square)
+function createGrid(dimensions = 16) {
+    for (let i = 0; i < dimensions; i++) {
+        const row = document.createElement('div')
+        container.appendChild(row)
+        for (let i = 0; i < dimensions; i++) {
+            const square = document.createElement('div')
+            row.appendChild(square)
+            hoverHighlight(square)
+        }
     }
 }
+
+// Create the first 16x16 grid
+createGrid()
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    let dims = prompt('How many rows would you like in the square grid?', '1-100')
+    if (dims === null) return  // Escape if user cancels the prompt
+    
+    // Make sure proper input is given
+    while (isNaN(dims) || dims < 1 || dims > 100) {
+        dims = prompt('Unsuitable input given. Try again.', '1-100')
+        if (dims === null) return
+    }
+
+    // Remove the original grid
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+
+    createGrid(dims)
+})
