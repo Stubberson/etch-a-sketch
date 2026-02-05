16f4596f1334b9ab8301d175c90ba5fe0183bd3c
Stubberson <tuukka.j.korpela@gmail.com>
2026-02-05 15:56:05 +0100

    Base grid and hover etching done

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const container = document.querySelector('div')
+const container = document.querySelector('.container')
 
+function hoverHighlight(square) {
+    square.addEventListener('mouseover', (event) => {
+        event.target.classList.add('highlight')
+    })
+}
+
+// Create a 16x16 grid
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div')
+    container.appendChild(row)
+    for (let i = 0; i < 16; i++) {
+        const square = document.createElement('div')
+        row.appendChild(square)
+        hoverHighlight(square)
+    }
+}
