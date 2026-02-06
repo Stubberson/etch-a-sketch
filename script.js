9de2908bb18f883860b68e2ec4a1f883a1a4fbcf
Stubberson <tuukka.j.korpela@gmail.com>
2026-02-06 12:21:22 +0100

    Add color mixing
    The mix calculation is still simple/faulty. I would have to know how
    rgb colors actually "mix" mathematically.

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const container = document.querySelector('.container')
 
+// Customize grid button
+const button = document.querySelector('button')
+button.focus()
+
+// Color buttons
+const colorButtons = document.querySelectorAll('.color')
+const buttonHeight = getComputedStyle(colorButtons.item(0))['height']
+let chosenColor = null
+
 // Hover highlights a grid square
 function hoverHighlight(square) {
     square.addEventListener('mouseover', (event) => {
         event.target.classList.add('highlight')
 
         // Add to opacity
-        let currentOpacity = parseFloat(getComputedStyle(event.target).opacity)
+        let currentOpacity = parseFloat(getComputedStyle(event.target)['opacity'])
         currentOpacity += 0.1
         event.target.style.setProperty('--highlight-opacity', currentOpacity)
+
+        // Mix colors
+        let currentColor = getComputedStyle(event.target)['background-color']
+        let currentColorValues = currentColor.match(/\d+/g)  // Parses the RGB for (d)igits (g)lobally
+        let [r, g, b] = currentColorValues.map(Number)  // Turn to numbers
+        
+        // No need to mix if no color is changed
+        if (chosenColor !== null) {
+            let chosenColorValues = chosenColor.match(/\d+/g)
+            let [cr, cg, cb] = chosenColorValues.map(Number)
+            let combination = `rgb(${cr - r}, ${cg - g}, ${cb - b})`
+            event.target.style.setProperty('--highlight-color', combination)
+        }
     })
 }
 
-let squares = []  // Container for all the square in the grid
+let squares = []  // Allows to modify each square
 function createGrid(dimensions = 16) {
     for (let i = 0; i < dimensions; i++) {
         const row = document.createElement('div')
@@ -29,9 +51,7 @@ function createGrid(dimensions = 16) {
 // Create the first 16x16 grid
 createGrid()
 
-// Define custom grid dimensions
-const button = document.querySelector('button')
-button.focus()
+// Custom grid dimensions
 button.addEventListener('click', () => {
     let dims = prompt('How many rows would you like in the square grid?', '1-100')
     if (dims === null) return  // Escape if user cancels the prompt
@@ -50,18 +70,15 @@ button.addEventListener('click', () => {
     createGrid(dims)
 })
 
-// Square color buttons
-const colorButtons = document.querySelectorAll('.color')
-const buttonHeight = getComputedStyle(colorButtons.item(0)).height
-
+// Change color
 colorButtons.forEach( btn => {
-    btn.style.setProperty('--button-width', buttonHeight)
-    // Change the color for the etch
+    btn.style.setProperty('--button-width', buttonHeight)  // First set height
     btn.addEventListener('click', () => {
+        chosenColor = getComputedStyle(btn)['background-color']
         squares.forEach( sqr => {
-            // Don't change the color for the already colored squares
+            // Don't change the highlight color for already colored squares
             if (!sqr.classList.contains('highlight')) {
-                sqr.style.setProperty('--highlight-color', getComputedStyle(btn).backgroundColor)
+                sqr.style.setProperty('--highlight-color', chosenColor)
             }
         })
     })
